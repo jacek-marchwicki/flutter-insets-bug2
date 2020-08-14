74e17edc48c4045e29cfee6524e4f86f979be9f0
Jacek Marchwicki <jacek.marchwicki@gmail.com>
2020-08-14 11:38:40 +0200

    Bug reproduction

--- android/app/src/main/kotlin/com/example/insetstest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/insetstest/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.insetstest
 
+import android.graphics.Color
+import android.os.Build
+import android.view.View
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-
+class MainActivity: FlutterActivity() {
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.navigationBarColor = Color.TRANSPARENT
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
 }
